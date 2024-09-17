6c20ff67022d295060ed20f6cd3f1437582cd187
Yahya Mohamed <yahya.hm750@gmail.com>
2024-09-17 17:33:32 +0000

    add opponent selection function

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Step 1
+Determine whether to play against computer or another player
+"""
+def chooseOpponent():
+    print("Would you like to play against the computer or another player")
+    print("Type 'c' for computer")
+    print("Type 'p' for player")
+
+    opponent = input("Enter: ")
+
+    if opponent.lower() == "c":
+        #play against computer function goes here
+        print("Your opponent will be the computer\n")
+    elif opponent.lower() == "p":
+        #play against player function goes here
+        print("Your opponent will be a player\n")
+    else:
+        print("Incorrect input. Please enter one of the valid options provided\n")
+        chooseOpponent()
+
+chooseOpponent()
